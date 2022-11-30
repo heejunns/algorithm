e42511ef7769e0c47ba87b67f4a94499b7a60d6e
heejunns <wns2252@hanmail.net>
2022-11-30 11:12:53 +0900

    bfs 연습코드

--- BFSsource.py
+++ BFSsource.py
@@ -4,7 +4,6 @@ node = [[], [2, 3, 8], [1, 7], [1, 4, 5], [3, 5], [3, 4], [7], [2, 6, 8],[1, 7]]
 visited = [False]* 9
 
 def bfs(node, start, visited):
-
     queue = deque([start])
 
     visited[start] = True
@@ -19,4 +18,5 @@ def bfs(node, start, visited):
                 visited[l] = True
 
 bfs(node,1,visited)
+
 
