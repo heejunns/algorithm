81061c0dcbd4d5b8b12fc754cbd5787be72ba0c8
heejunns <wns2252@hanmail.net>
2022-12-14 12:46:26 +0900

    [Programmers] 숨어있는 숫자의 덧셈 (1) (level 0)

--- .vscode/Programmers_40_.py
+++ .vscode/Programmers_40_.py
@@ -1,4 +1,4 @@
-# 프로그래머스 숨어있는 숫자의 덧셈 (1)
+# 프로그래머스 숨어있는 숫자의 덧셈 (1) (level 0)
 from string import ascii_letters
 def solution(my_string):
     x = list(ascii_letters)
@@ -7,4 +7,5 @@ def solution(my_string):
         if i not in x:
             answer+= int(i)
     return answer
+
 
