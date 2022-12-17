2e2e1176c69b9da23feb266252ce5f5439e961f4
heejunns <wns2252@hanmail.net>
2022-12-17 10:54:32 +0900

    [Programmers] 외계행성의 나이 (level 0)

--- .vscode/programmers_62_.py
+++ .vscode/programmers_62_.py
@@ -1,7 +1,8 @@
-# 프로그래머스 외계행성의 나이 (level):
+# 프로그래머스 외계행성의 나이 (level 0)
 def solution(age):
     answer = ""
     x = {0:"a",1:"b",2:"c",3:"d",4:"e",5:"f",6:"g",7:"h",8:"i",9:"j"}
     for i in str(age):
         answer += x[int(i)]
     return answer
+print(solution(123))
