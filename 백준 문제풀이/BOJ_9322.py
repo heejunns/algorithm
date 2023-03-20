634f641a6765fc881abcf63b1cf5f0c1f9df4cdf
heejunns <wns2252@hanmail.net>
2023-03-20 23:57:37 +0900

    백준 9322 번, 철벽 보안 알고리즘

---
+++ 백준 문제풀이/BOJ_9322.py
@@ -0,0 +1,14 @@
+# 백준 9322 번, 철벽 보안 알고리즘
+import sys
+from collections import deque
+n = int(sys.stdin.readline())
+for i in range(n):
+    result = []
+    str_number = int(sys.stdin.readline())
+    for j in range(3):
+        if (j == 2):
+                a = list(map(str,sys.stdin.readline().rstrip().split()))
+    
+                
+    
+
