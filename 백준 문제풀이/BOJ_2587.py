7eccf1159fc92ecdc31f0fc0dd20d65f7a84f169
heejunns <wns2252@hanmail.net>
2023-05-16 23:56:12 +0900

    [BOJ] 2587번 대표값2

---
+++ 백준 문제풀이/BOJ_2587.py
@@ -0,0 +1,12 @@
+# 백준 2587번 대표값2
+
+import sys
+total = []
+sum = 0
+for i in range(5):
+    n = int(sys.stdin.readline())
+    sum+=n
+    total.append(n)
+total.sort()
+print(int(sum/5))
+print(total[2])
