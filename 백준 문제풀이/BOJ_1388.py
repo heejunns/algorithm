22adafba655d7bec76c158ca928b24266b817eb6
heejunns <wns2252@hanmail.net>
2023-05-20 23:57:53 +0900

    [BOJ] 백준 1388번 바닥 장식

--- 백준 문제풀이/BOJ_1388.py
+++ 백준 문제풀이/BOJ_1388.py
@@ -1,6 +1,29 @@
 # 백준 1388번 바닥 장식
-visited = [[False for i in range(5)]for l in range(5)]
-visited[0][0] = True
-print(visited)
+import sys
+n,m = map(int,sys.stdin.readline().rstrip().split())
+tai = [] 
+for i in range(n):
+    input = list(sys.stdin.readline().strip())
+    tai.append(input)
+count = 0
+for i in range(n):
+    s = 0
+    for l in range(m):
+        if tai[i][l] == "-" and s == 0:
+            s = 1
+            count+=1
+        elif tai[i][l] == "|":
+            s = 0
+for i in range(m):
+    s = 0
+    for l in range(n):
+        if tai[l][i] == "|" and s == 0:
+            s = 1
+            count+=1
+        elif tai[l][i] == "-":
+            s = 0
+print(count)
 
+            
+    
 
